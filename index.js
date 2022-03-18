31941bba575a188362c6d3f853f904bb9f8ce4df
zeynepdaglar <zeynepdaglarx@gmail.com>
2022-03-18 19:37:39 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,13 @@ function active(item){
     $("#menuDiv div").removeClass("bluebox");
     $("#menuDiv a").removeClass("blueText");
     $(item).addClass("blueLine");
-    $(item).children().addClass("bluebox");
+    $(item).find("div").addClass("bluebox");
     $(item).find("a").addClass("blueText");
 }
 
+function mobilActive(item){
+$("#mobilMenuDiv div").removeClass("mobilBlueLine");
+// $(item).find("div").removeClass("mobilGreyLine");
+$(item).find("div").addClass("mobilBlueLine");
+
+}
