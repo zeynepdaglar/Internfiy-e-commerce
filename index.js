df9e34e3e06ea265602f172ba7859d4d7d81445c
zeynepdaglar <zeynepdaglarx@gmail.com>
2022-03-20 13:16:29 +0300

    Update index.js

--- index.js
+++ index.js
@@ -13,3 +13,13 @@ function mobilActive(item){
 $("#mobilMenuDiv div").removeClass("mobilBlueLine");
 $(item).addClass("mobilBlueLine");
 }
+
+function Toast(item) {
+  $(item).empty().append("Ürün Özelliği Seç");
+  var toastTrigger = document.getElementById('liveToastBtn')
+  var toastLiveExample = document.getElementById('liveToast')
+  if (toastTrigger) {
+  var toast = new bootstrap.Toast(toastLiveExample)
+  toast.show()
+  }
+}
